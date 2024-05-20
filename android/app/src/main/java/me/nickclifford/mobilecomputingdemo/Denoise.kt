52440ef6bca1d267fa06a913a9974d200432234d
Nick Clifford <nick@nickclifford.me>
2024-05-20 16:27:17 -0500

    proper navigation

---
+++ android/app/src/main/java/me/nickclifford/mobilecomputingdemo/Denoise.kt
@@ -0,0 +1,9 @@
+package me.nickclifford.mobilecomputingdemo
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DenoisePage() {
+    Text("Denoise page!")
+}
